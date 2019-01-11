2391fc5cd442d3814fc6d0ee9af46f0b388d7ec4
ffzhello <314094371@qq.com>
2019-01-11 21:13:22 +0800

    日志文件存入MongoDB

---
+++ src/main/java/cn/edu/jslab6/autoresponse/forensictask/MongoDbManager.java
@@ -0,0 +1,10 @@
+package cn.edu.jslab6.autoresponse.forensictask;
+
+/**
+ * @description: MongoDb数据库管理
+ * @author: ffzheng
+ * @create: 2019-01-11 21:11:58
+ **/
+public class MongoDbManager {
+
+}
