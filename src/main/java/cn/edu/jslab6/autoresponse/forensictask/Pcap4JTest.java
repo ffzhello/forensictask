8603b484e8b8cec933babb031bebbeb6fae6fd74
ffzhello <314094371@qq.com>
2019-04-04 09:55:53 +0800

    日志提取字段修改

--- src/main/java/cn/edu/jslab6/autoresponse/forensictask/Pcap4JTest.java
+++ src/main/java/cn/edu/jslab6/autoresponse/forensictask/Pcap4JTest.java
@@ -3,6 +3,7 @@ package cn.edu.jslab6.autoresponse.forensictask;
 import org.pcap4j.core.*;
 import org.pcap4j.packet.Packet;
 import java.io.File;
+import java.sql.Timestamp;
 
 /**
  * Created by ffzheng on 2018/6/6.
@@ -13,21 +14,24 @@ public class Pcap4JTest {
 
             int len = 0;
             int size = 0;
-            PcapHandle handle = Pcaps.openOffline("E://0.pcap");
-            PcapDumper dumper = handle.dumpOpen("E://123.pcap");
+            PcapHandle handle = Pcaps.openOffline("E://1.pcap");
+            //PcapDumper dumper = handle.dumpOpen("E://123.pcap");
             Packet p ;
             while ((p = handle.getNextPacket()) != null) {
                 len += p.length();
                 size += p.getRawData().length;
-                dumper.dump(p, handle.getTimestamp());
+                //dumper.dump(p, handle.getTimestamp());
+                Timestamp timestamp = handle.getTimestamp();
+                System.out.println(timestamp);
             }
 
-            //
+            /**
             if(dumper != null) {
                 System.out.println(dumper);
                 dumper.close();
                 System.out.println(dumper);
             }
+             **/
 
             if(handle != null)
                 handle.close();
--- src/main/java/cn/edu/jslab6/autoresponse/forensictask/TimeManager.java
+++ src/main/java/cn/edu/jslab6/autoresponse/forensictask/TimeManager.java
@@ -1,5 +1,6 @@
 package cn.edu.jslab6.autoresponse.forensictask;
 
+import java.math.BigDecimal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -37,7 +38,21 @@ public class TimeManager {
     }
 
     public static void main(String[] args) {
-        System.out.println(changeTsToString("1546676692.385804"));
-        System.out.println(changeTsToString("1546676699.976212"));
+        double a = Double.parseDouble("1547639390.257149");
+        double b = Double.parseDouble("0.000005");
+        BigDecimal c = BigDecimal.valueOf(a+b);
+        System.out.println(c);
+        System.out.println(changeTsToString(String.valueOf(c)));
+      // System.out.println(changeTsToString("1546676699.976212"));
+        System.out.println("sss,eee".contains(","));
+        String ssl = "imap,ssl";
+        String[] services = ssl.split(",");
+        String ss = "";
+        for (String service: services) {
+            if (BroLogType.protocolLogSet.contains(service+".log")) {
+                ss += service + ",";
+            }
+        }
+        System.out.println(ss);
     }
 }
